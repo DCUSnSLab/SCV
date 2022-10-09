cb3c29279c6aa61a03e297097013af2eea4e46cc
marsberry <marsberry@cu.ac.kr>
2022-10-09 23:39:25 +0900

    AD-147 first test for controller using morai sim

--- src/scv_system/scv_control/src/controller/controller.py
+++ src/scv_system/scv_control/src/controller/controller.py
@@ -1,4 +1,33 @@
-import test_package.hello
+import sys
+from math import pi
 
-test_package.hello.say('test')
-print('test')
+import rospy
+from morai_msgs.msg import CtrlCmd
+
+class moraiCtrl:
+    def __init__(self):
+        rospy.init_node('morai_ctrl', anonymous=True)
+
+        #arg = rospy.myargv(argv=sys.argv)
+
+        #publisher
+        self.ctrl_pub = rospy.Publisher('/ctrl_cmd', CtrlCmd, queue_size=1)
+
+        self.ctrl_msg = CtrlCmd()
+
+        #time rate
+        self.rate = rospy.Rate(30) #30hz
+        self.runCtrl()
+
+    def runCtrl(self):
+        while not rospy.is_shutdown():
+            self.ctrl_msg.velocity = 0.1
+            self.ctrl_msg.accel = 0.3
+            self.ctrl_msg.acceleration = 0
+            #self.ctrl_msg.steering = -30 / 180 * pi
+            self.ctrl_pub.publish(self.ctrl_msg)
+            print('target vel {}, acc {}'.format(self.ctrl_msg.velocity, self.ctrl_msg.accel))
+            self.rate.sleep()
+
+if __name__ == '__main__':
+    mct = moraiCtrl()
--- src/scv_system/scv_control/src/controller/tt.py
+++ src/scv_system/scv_control/src/controller/tt.py
@@ -1,4 +1,9 @@
 from test_package.hello import Hello
+import rospy
 
 he = Hello()
 he.say('test')
+rospy.init_node('test_node', anonymous=True)
+rate = rospy.Rate(30)
+while not rospy.is_shutdown():
+    rate.sleep()
