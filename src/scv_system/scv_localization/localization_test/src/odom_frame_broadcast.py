7b081652b1f88ac9630ef47aaaa16b3e9407b964
totwjfakd <totwjfakd@gmail.com>
2025-02-15 23:38:28 +0900

    add imu gps localization

---
+++ src/scv_system/scv_localization/localization_test/src/odom_frame_broadcast.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+import rospy
+import tf2_ros
+from geometry_msgs.msg import TransformStamped
+from nav_msgs.msg import Odometry
+
+def handle_odom_pose(msg, br):
+    t = TransformStamped()
+    t.header.stamp = msg.header.stamp
+    t.header.frame_id = "odom_utm"
+    t.child_frame_id = "base_link"
+    t.transform.translation.x = msg.pose.pose.position.x
+    t.transform.translation.y = msg.pose.pose.position.y
+    t.transform.translation.z = msg.pose.pose.position.z
+    t.transform.rotation = msg.pose.pose.orientation
+
+    br.sendTransform(t)
+
+if __name__ == '__main__':
+    rospy.init_node('odom_to_base_link_broadcaster')
+    br = tf2_ros.TransformBroadcaster()
+    rospy.Subscriber('/odom/coordinate/gps', Odometry, handle_odom_pose, br)
+    rospy.spin()
