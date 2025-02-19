cfa6c7b9acf89e3ef93490bcb46d897d3df6a542
totwjfakd <totwjfakd@gmail.com>
2025-02-19 16:20:13 +0900

    중간 병합을 위한 커밋

--- src/scv_system/scv_localization/localization_test/src/odom_frame_broadcast.py
+++ src/scv_system/scv_localization/localization_test/src/odom_frame_broadcast.py
@@ -19,5 +19,7 @@ def handle_odom_pose(msg, br):
 if __name__ == '__main__':
     rospy.init_node('odom_to_base_link_broadcaster')
     br = tf2_ros.TransformBroadcaster()
-    rospy.Subscriber('/odom/coordinate/gps', Odometry, handle_odom_pose, br)
+    #rospy.Subscriber('/odom/coordinate/gps', Odometry, handle_odom_pose, br)
+    #rospy.Subscriber('scv_odom', Odometry, handle_odom_pose, br)
+    rospy.Subscriber('zed_node/odom', Odometry, handle_odom_pose, br)
     rospy.spin()
