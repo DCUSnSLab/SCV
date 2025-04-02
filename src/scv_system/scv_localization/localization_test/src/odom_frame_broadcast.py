0c7be499aa3295dbba956425663495f12fa49986
totwjfakd <totwjfakd@gmail.com>
2025-04-03 06:03:27 +0900

    push prediction branch

--- src/scv_system/scv_localization/localization_test/src/odom_frame_broadcast.py
+++ src/scv_system/scv_localization/localization_test/src/odom_frame_broadcast.py
@@ -19,8 +19,8 @@ def handle_odom_pose(msg, br):
 if __name__ == '__main__':
     rospy.init_node('odom_to_base_link_broadcaster')
     br = tf2_ros.TransformBroadcaster()
-    #rospy.Subscriber('/odom/coordinate/gps', Odometry, handle_odom_pose, br)
-    rospy.Subscriber('/odom/ekf', Odometry, handle_odom_pose, br)
+    rospy.Subscriber('/odom/coordinate/gps', Odometry, handle_odom_pose, br)
+    #rospy.Subscriber('/odom/ekf', Odometry, handle_odom_pose, br)
     #rospy.Subscriber('scv_odom', Odometry, handle_odom_pose, br)
     #rospy.Subscriber('zed_node/odom', Odometry, handle_odom_pose, br)
     #rospy.Subscriber('/odom/coordinate/imu', Odometry, handle_odom_pose, br)
