4e9dc423421e185e98dd91352c37cabfe1f0112c
nevlife <pcdpcd100@gmail.com>
2025-07-30 00:04:13 +0900

    fix rtk

--- src/vehicle/hunter2/launch/robot.launch.py
+++ src/vehicle/hunter2/launch/robot.launch.py
@@ -15,7 +15,7 @@ def generate_launch_description():
     declared_arguments = [
         DeclareLaunchArgument(
             "description_file",
-            default_value="robot.urdf.xacro",
+            default_value="robot_main.urdf.xacro",
             description="URDF/XACRO description file with the robot.",
         ),
         DeclareLaunchArgument(
